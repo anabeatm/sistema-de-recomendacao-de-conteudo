e88bb102068cff20f6675560feabc6c064b79483
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2025-10-11 17:16:35 -0300

    feat(UserDAO): add findByEmail method

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,4 +1,23 @@
 package dao;
 
-public class UserDAO {
+import domain.User;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+public class UserDAO extends AbstractDAO<User, Long> {
+
+    public UserDAO(EntityManager em) {
+        super(em);
+    }
+
+    public User findByEmail(String email) {
+        try {
+            TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.user_email = :email", User.class);
+            query.setParameter("email", email);
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
