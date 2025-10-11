0e39563f20df603ce533834c2a3c738893a5c549
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2025-10-11 17:49:16 -0300

    feat(RecommendationService): implement RecommendationService still need to continue

--- src/main/java/service/RecommendationService.java
+++ src/main/java/service/RecommendationService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dao.*;
+import domain.*;
+
+import java.util.List;
+
+
 public class RecommendationService {
+    private final UserDAO userDAO;
+    private final RatingDAO ratingDAO;
+
+    public RecommendationService(UserDAO userDAO, RatingDAO ratingDAO) {
+        this.userDAO = userDAO;
+        this.ratingDAO = ratingDAO;
+    }
+
+    public List<Item> getRecommendationForUser(Long userID) {
+        User targetUser = userDAO.searchByID(userID);
+        if(targetUser == null) {
+            throw new IllegalArgumentException("User not found!");
+        }
+//      TODO implementar o método de recomendação
+        return List.of();
+    }
 }
