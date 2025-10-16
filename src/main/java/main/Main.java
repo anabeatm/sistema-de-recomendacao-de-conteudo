5907cd0a018e51b6ecaf1d7bd48ca475688513c2
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2025-10-15 22:46:20 -0300

    test(Main): add Item entity tests

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,8 @@
 package main;
 
+import dao.ItemDAO;
 import dao.UserDAO;
-import domain.User;
+import domain.*;
 import util.JPAUtil;
 
 import javax.persistence.EntityManager;
@@ -11,17 +12,24 @@ public class Main {
         EntityManager em = JPAUtil.getEntityManager();
 
         UserDAO userDAO = new UserDAO(em);
+        ItemDAO itemDAO = new ItemDAO(em);
 
         System.out.println("cadastrando alguns usuários para teste:");
         User ana = new User("Ana", "ana123@email.com");
         User pedro = new User("Pedro", "pedro123@email.com");
-        System.out.println("usuarios criados na classe User.");
 
-        userDAO.save(ana);
-        userDAO.save(pedro);
 
-        System.out.println(ana.getId());
-        System.out.println(pedro.getId());
+//        userDAO.save(ana);
+//        userDAO.save(pedro);
+//
+//        System.out.println(ana.getId());
+//        System.out.println(pedro.getId());
+        System.out.println("cadastrando alguns itens");
+        Item avalonMusic = new Music("Avalon", TypeItem.MUSIC, "KR&B", "DPR IAN", "Moodswings");
+        Film howToTrainYourDragonFilm = new Film("How to train your dragon", TypeItem.FILM, "Comédia", 120, "sei lá");
+
+
+        System.out.println(howToTrainYourDragonFilm.getFormattedDuration());
 
     }
 }
